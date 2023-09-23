628c63afdd4d05120a71f129217974868e3e44a4
Alndaly <71250227+Alndaly@users.noreply.github.com>
2023-09-23 07:48:57 +0000

    Deploying to gh-pages from @ Alndaly/alndaly.github.io@26520c0ce9065215ffc18888154cfac9f1b6aa46 🚀

---
+++ js/baidu.js
@@ -0,0 +1,6 @@
+(function (window, document, undefined) {
+    const meta = document.createElement('meta');
+    meta.name = 'baidu-site-verification';
+    meta.content = 'codeva-wDGrzQfHtQ';
+    document.getElementsByTagName('head')[0].appendChild(meta);
+})(window, document);
