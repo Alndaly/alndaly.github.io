3154c87db2ed381be65daf128c60dcd4980d7dc5
Kinda Hall <1142704468@qq.com>
2025-03-18 12:58:42 +0800

    fix: 🐛 修复sidebar

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -48,7 +48,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/Alndaly/alndaly.github.io/tree/main/packages/create-docusaurus/templates/shared/',
           remarkPlugins: [remarkMath],
           rehypePlugins: [rehypeKatex],
           showLastUpdateTime: true,
